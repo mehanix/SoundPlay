8c67f019e0f9bb320eb8eabffd9968fd7ba73f0e
Nicoleta Ciausu <nixikunn@gmail.com>
2018-01-14 03:51:07 +0200

    First commit

---
+++ MusicPlayer/MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Song";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove Song";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 62);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(478, 147);
+            this.listBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(255, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 44);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Start Playlist";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(336, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 44);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Skip Current Song";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Volume:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(386, 215);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Now Playing: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 255);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SoundPlay";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MusicPlayer/MusicPlayer/Form1.cs
@@ -0,0 +1,133 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        public WMPLib.WindowsMediaPlayer wplayer;
+        public IWMPPlaylist pl;
+        public struct song
+        {
+            public string path { get; set; }
+            public string name { get; set; }
+        }
+        BindingList<song> playlist;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            playlist = new BindingList<song>();
+            listBox1.DataSource = playlist;
+            listBox1.DisplayMember = "name";
+            listBox1.ValueMember = "path";
+            
+            wplayer = new WindowsMediaPlayer();
+            
+            wplayer.CurrentItemChange += Wplayer_CurrentItemChange;
+            wplayer.settings.volume = 50;
+        }
+
+        private void Wplayer_CurrentItemChange(object pdispMedia)
+        {
+           // MessageBox.Show("AAAAAAAAAAAA ");
+
+            int index=0;
+            for (int i = 0; i < playlist.Count - 1; i++)
+            {
+                if (wplayer.currentMedia.isIdentical[pl.Item[i]])
+                {
+                    index = i;
+                    
+                    break;
+                }
+            }
+
+            listBox1.SelectedIndex = index;
+            label2.Text = "Now playing: " + wplayer.currentMedia.name;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+     
+            opf.ShowDialog();
+            
+            MessageBox.Show(opf.FileName);
+            song selection = new song();
+            selection.path = opf.FileName;
+            selection.name = opf.SafeFileName;
+            
+            playlist.Add(selection);
+            //wplayer.URL = opf.FileName;
+            //wplayer.controls.play();
+
+        }
+
+        private void wplayer_PositionChange(object sender, _WMPOCXEvents_PositionChangeEvent e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.SelectedValue.ToString());
+            wplayer.URL = listBox1.SelectedValue.ToString();
+            wplayer.controls.play();
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.stop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //scos iteme din playlistul vechi
+
+            pl = wplayer.newPlaylist("pl","");
+            wplayer.currentPlaylist = pl;
+           
+            foreach (song s in playlist)
+            {
+                //creat nou obiect "media"
+                IWMPMedia sng = wplayer.newMedia(s.path);
+                //adaugat la playlistul curent :D
+                wplayer.currentPlaylist.appendItem(sng);
+            }
+            wplayer.controls.play();
+            //listBox1.SelectedIndex = 0;
+            
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            wplayer.controls.next();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            playlist.RemoveAt(listBox1.SelectedIndex);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //MessageBox.Show(trackBar1.Value.ToString());
+            wplayer.settings.volume = trackBar1.Value;
+        }
+    }
+}
